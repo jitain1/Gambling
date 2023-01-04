cf701e5218695cdd77cf4a4ba6e8f44fe49ec2a2
jitain1 <singhjitainj@gmail.com>
2023-01-04 23:58:17 +0530

    Welcome to Gambling Program

---
+++ src/com/bridgelabz/GamblingSimulation.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class GamblingSimulation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Gambling Simulation Program");
+
+	}
+
+}
