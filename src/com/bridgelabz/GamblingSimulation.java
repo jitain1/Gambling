ff463973338d22d8be26ee488b0b44a3804e67db
jitain1 <singhjitainj@gmail.com>
2023-01-05 00:44:47 +0530

    Initail Stake and bet

--- src/com/bridgelabz/GamblingSimulation.java
+++ src/com/bridgelabz/GamblingSimulation.java
@@ -1,6 +1,8 @@
 package com.bridgelabz;
 
 public class GamblingSimulation {
+	public static int INITAIL_STAKE = 100;
+	public static int BET = 1;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Gambling Simulation Program");
