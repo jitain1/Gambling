0db344146f407193796c59d92c11b74c30108835
jitain1 <singhjitainj@gmail.com>
2023-01-09 15:37:00 +0530

    Gambler will Resign for day if looses

--- src/com/bridgelabz/GamblingSimulation.java
+++ src/com/bridgelabz/GamblingSimulation.java
@@ -4,8 +4,7 @@ public class GamblingSimulation {
 	
 	static int stake = 100;   
 	static final int BET = 1;
-	
-/* 1 bet for win or loose */
+
 	public static void checkWinOrLoose() {
 		int random_Check = (int) (Math.random()*2);
 		switch(random_Check) {
@@ -21,10 +20,28 @@ public class GamblingSimulation {
 		}	
 	}
 	
+/*Gambler will resign for the day if won or lost 50% of the stake */
+	public static void gambling() {
+		double winningTarget = stake * 1.5;
+		double loosingTarget = stake * 0.5;
+	
+		System.out.println("Starting with : " +stake + "    Winning target = " + winningTarget + "    Loosing target = " + loosingTarget);
+		
+		while(stake <= winningTarget && stake >= loosingTarget) {
+			checkWinOrLoose();
+			System.out.println("\n");
+			
+			if(stake == 150 || stake == 50) {
+				System.out.println("***************************** Target Reached  *******************************");
+				break;
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 		
 		System.out.println("***************** WELCOME TO GAMBLING SIMULATOR PROBLEM ****************\n");
-		checkWinOrLoose();
+		gambling();
 	}
 
 }
